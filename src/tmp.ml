6e2fdae45bdb192f8c111048512eab357b7b869a
Tim Cuthbertson <tim@gfxmonk.net>
2025-01-29 16:51:07 +1100

    add OPAM2NIX_LEAVE_TMP

--- src/tmp.ml
+++ src/tmp.ml
@@ -2,5 +2,9 @@ let with_temp_file : 'a. ((string * out_channel) -> 'a Lwt.t) -> 'a Lwt.t = fun 
   let (path, out_channel) = Filename.open_temp_file "opam2nix-" "" in
   Lwt.finalize
     (fun () -> fn (path, out_channel))
-    (fun () -> Lwt.return (Unix.unlink path))
+    (fun () -> Lwt.return (
+      if Sys.getenv_opt "OPAM2NIX_LEAVE_TMP" = Some "true"
+        then ()
+        else Unix.unlink path
+    ))
 
