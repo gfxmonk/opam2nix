a4d968c3b49a6b360aa519ef4a3a7f0e4cf522e0
Tim Cuthbertson <tim@gfxmonk.net>
2025-01-29 11:40:54 +1100

    remove unnecessary $TEMPDIR use

--- src/tmp.ml
+++ src/tmp.ml
@@ -1,6 +1,5 @@
 let with_temp_file : 'a. ((string * out_channel) -> 'a Lwt.t) -> 'a Lwt.t = fun fn ->
-  let temp_dir = Sys.getenv_opt "TMPDIR" in
-  let (path, out_channel) = Filename.open_temp_file ?temp_dir "opam2nix-" "" in
+  let (path, out_channel) = Filename.open_temp_file "opam2nix-" "" in
   Lwt.finalize
     (fun () -> fn (path, out_channel))
     (fun () -> Lwt.return (Unix.unlink path))
